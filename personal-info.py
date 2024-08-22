1c16227679097e7a06a93b9ea47ad029c90eb340
NetAcad DEVASC <changeme@example.com>
2024-08-22 14:22:45 +0000

    finished

---
+++ personal-info.py
@@ -0,0 +1,6 @@
+firstname = input("What is your first name? ")
+lastname = input("What is your last name? ")
+location = input("What is your location? ")
+age = input ("What is your age? ")
+print("Hi " + firstname + " " + lastname +"!" + " " + "Your location is " + " " + location  + " " + "and you are " + age +".")
+
